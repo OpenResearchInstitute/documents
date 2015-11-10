551978f3bc2379b8860d6c59a1e0bed382c1b852
Abraxas3d <abraxas@sand.net>
2015-11-10 09:38:02 -0800

    QSLFractal - parameterizing
    Trying to make the fractals parameterized based on QSO type data. First
    steps in that direction.

--- Visualizations/QSLtree_params.py
+++ Visualizations/QSLtree_params.py
@@ -1,28 +1,36 @@
 import pygame, math
- 
+width = 550*2
+height = 350*2
+num_contacts = 9
+
+
 pygame.init()
-window = pygame.display.set_mode((600, 600))
+window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Fractal Tree")
 screen = pygame.display.get_surface()
  
+
+
+
+
 def drawTree(x1, y1, angle, depth):
     if depth:
         x2 = x1 + int(math.cos(math.radians(angle)) * depth * 10.0)
         y2 = y1 + int(math.sin(math.radians(angle)) * depth * 10.0)
         pygame.draw.line(screen, (255,255,255), (x1, y1), (x2, y2), 2)
         drawTree(x2, y2, angle - 40, depth - 1)
-        drawTree(x2, y2, angle + 20, depth - 1)
+        drawTree(x2, y2, angle + 40, depth - 1)
  
 def input(event):
     if event.type == pygame.QUIT:
         exit(0)
  
-drawTree(300, 550, -90, 8)
+drawTree(width/2, height - 50, -90, num_contacts)
 
 
-#pygame.display.flip()
+pygame.display.flip()
 
-pygame.image.save(screen, "QSLtree.png")
+pygame.image.save(screen, "QSLtree_params.png")
 
 
 while True:
