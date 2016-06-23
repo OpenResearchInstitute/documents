fc04ad2c944ffe2c84c89acf66de1efa3bf2dbb6
Abraxas3d <abraxas@sand.net>
2016-06-22 17:56:31 -0700

    removed struct and simplified comments

--- Engineering/Requirements/Software/GSE_packet.py
+++ Engineering/Requirements/Software/GSE_packet.py
@@ -1,30 +1,13 @@
-#this is what I used first
-from struct import *
-import binascii
-
 #this is what was recommended by mossman
 from bitstring import BitArray, BitStream
 
 
-##Crash course in struct:
-#>>> from struct import *
-#>>> pack('hhl', 1, 2, 3)
-#'\x00\x01\x00\x02\x00\x00\x00\x03'
-#>>> unpack('hhl', '\x00\x01\x00\x02\x00\x00\x00\x03')
-#(1, 2, 3)
-#>>> calcsize('hhl')
-#8
-#>>> pack('!bbb', 1, 2, 3)
-#'\x01\x02\x10'
 
-##GSE header
-#Start indicator one bit
-#End indicator one bit
-#label type indicator two bits
-#there's GSE-length, which is 12 bits
-#16 bits is two bytes
-#so the possible combinations?
-#11101111 is 15, 16
+#Baseband frame information
+#should this be an object?
+#N1 is the number of bytes until the end of the Base Band frame.
+N1 = 10
+
 
 #if start indicator == 0 and end indicator == 0 and label type indicator == 0
 # then there are four padding bits, and padding bytes?
@@ -32,10 +15,11 @@ from bitstring import BitArray, BitStream
 
 #Protocol Data Units are the things we want to encapsulate
 #maybe these are a bitstream? Probably not? 
+#How about making it a random value every time we run the code?
 
 default_start = '0b1'
-default_stop = '0b1'
-default_label_type = '0b11'
+default_stop = '0b0'
+default_label_type = '0b01'
 #00 6-byte label is present and shall be used for filtering
 #01 3-byte label is present and shall be used for filtering
 #10 Broadcast. No label field present. All Rx shall process this packet.
@@ -47,22 +31,26 @@ default_label_type = '0b11'
 #   of GSE packets with the same label without repeating the label field. 
 #   This value shall not be used for the first GSE packet in the frame. 
 
-default_GSE_length = '0b000000000000'
+default_GSE_length = '0b101010101010'
 
+default_fragment_ID = '0b01010101'
 
-default_fragment_ID = '0b11111111'
+default_total_length = '0b1110111011101110'
 
-default_total_length = '0b0000000011111111'
-
-default_protocol_type = '0b0000000011111111'
+default_protocol_type = '0b00100010001000100010'
 
 #default_label presence and length depends on the label type
 if default_label_type == '0b00':
-	default_label = '0b000000001111111100000000111111110000000011111111'
+	print "label type is 00"
+	default_label = '0b101011010101010110101101010101011010110101010101'
+	print "Six byte label"
 elif default_label_type == '0b01':
-	default_label = '0b000000001111111100000000'
+	print "label type is 01"
+	default_label = '0b101011010101010101010101'
+	print "three byte label"
 else:
 	default_label = None
+	print "label type was not 00 or 01. That means no label."
 
 
 
@@ -84,26 +72,58 @@ default_extension_header_1 = zero_prefix+H_LEN+H_TYPE
 class PDU:
 	def __init__(self):
 		start = BitArray(default_start)
+		print "start is ", start
 		stop = BitArray(default_stop)
+		print "stop is ", stop
 		label_type = BitArray(default_label_type)
+		print "label type is", label_type
+
+
+
+		#they have to exist before we can use them
+		padding = BitArray()
+		fragment_ID = BitArray()
+		total_length = BitArray()	
+		protocol_type = BitArray()	
+		label = BitArray()
+
+
 		
-		if start == '0b1' && end == '0b0' && label_type == '0b00':
+		if start == '0b0' and stop == '0b0' and label_type == '0b00':
+			print "padding condition has been met"
 
 #N1 is the number of bytes until the end of the Base Band frame.
-		Padding_bits
-		for(i=0;i<N1;i++) {
-		Padding_bytes
-		
-
-		}
-		
+#how do we get N1? Keep track of it with calculations.
+			for x in range(0, N1-1):{
+			padding.append('0b00000000')
+			}
+			
 		
 		GSE_length = BitArray(default_GSE_length)
+		
+		
+		if start == '0b0' and stop == '0b0':
+			fragment_ID = BitArray(default_fragment_ID)
+			print "fragment ID condition has been met and fragment ID value is", fragment_ID
+		
+		if start == '0b1' and stop == '0b0':
+			total_length = BitArray(default_total_length)
+			print "total length field condition has been met and total length value is", total_length
+		
+		if start == '0b1':
+			protocol_type = BitArray(default_protocol_type)
+			print "protocol type field condition has been met and protocol type value is", protocol_type
+			if label_type == '0b00':
+				label = BitArray(default_label)
+				print "six byte label condition has been met and the label is", label
+			elif label_type == '0b01':
+				label = BitArray(default_label)
+				print "three byte label condition has been met and the label is", label
+
 
 		fixed_header = start+stop+label_type+GSE_length
-		label = BitArray(default_label)
 
-		self.data = fixed_header+label
+		self.data = fixed_header+padding+fragment_ID+total_length+protocol_type+label
 		self.size = len(self.data)
 
 #Encapsulated Packet Unit = GSE Packet
