8d1bcdab12dae599806f3ef7c25793059bb76d6e
Paul Williamson <paul@mustbeart.com>
2020-12-12 21:47:48 -0800

    Add Python screenshot scripts for PS and SG

---
+++ Remote_Labs/Scripts/ps_screen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#
+# Get a screen image from the DP832 Power Supply
+# Open Research Institute -- Remote Lab West
+#
+# https://github.com/phase4ground/documents
+#
+import pyvisa as visa
+
+rm = visa.ResourceManager('@py')
+# ps = rm.open_resource('USB0::0x1AB1::0x0E11::DP8C222902347::INSTR')
+ps = rm.open_resource('TCPIP::dp832.sandiego.openresearch.institute::INSTR')
+print(ps.query('*IDN?'))
+
+screen_data = ps.query_binary_values(':SYST:PRINT? BMP',datatype='B',container=bytearray,delay=0.2)
+with open('ps_screen.bmp', 'wb') as f:
+    f.write(screen_data)
+print('ps_screen.bmp written')
---
+++ Remote_Labs/Scripts/sg_screen.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#
+# Get a screen image from the DSG821A Signal Generator
+# Open Research Institute -- Remote Lab West
+#
+# https://github.com/phase4ground/documents
+#
+import pyvisa as visa
+
+rm = visa.ResourceManager('@py')
+sg = rm.open_resource('TCPIP::dsg821a::INSTR')
+print(sg.query('*IDN?'))
+
+screen_data = sg.query_binary_values(':PRIV:SNAP? BMP',datatype='B',container=bytearray,delay=0.2)
+with open('sg_screen.bmp', 'wb') as f:
+    f.write(screen_data)
+print('sg_screen.bmp written')
